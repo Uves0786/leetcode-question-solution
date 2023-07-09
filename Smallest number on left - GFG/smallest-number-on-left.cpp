cca0dbc358df7e372c7efeafd3e9adf5d4fe9f2a
Mohd Uves Khan <114132833+Uves0786@users.noreply.github.com>
2023-07-09 12:09:42 +0530

    Added solution - LeetHub

---
+++ Smallest number on left - GFG/smallest-number-on-left.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<int> leftSmaller(int n, int arr[]){
+        vector<int>v1;
+        stack<int>s;
+        for(int i=0;i<n;i++)
+        {
+            if(s.size()==0)
+            {
+                v1.push_back(-1);
+            }
+            else if(s.size()>0 && s.top()<arr[i])
+            {
+                v1.push_back(s.top());
+            }
+            else if(s.size()>0 && s.top()>=arr[i])
+            {
+                while(s.size()>0 && s.top()>=arr[i])
+                {
+                    s.pop();
+                }
+                if(s.size()==0)
+                {
+                    v1.push_back(-1);
+                }
+                else
+                {
+                    v1.push_back(s.top());
+                }
+            }
+            s.push(arr[i]);
+        }
+        return v1;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i = 0;i < n;i++)
+            cin>>a[i];
+        
+        Solution ob;
+        vector<int> ans = ob.leftSmaller(n, a);
+        for(int i = 0;i < n;i++)
+            cout<<ans[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
