7aa6b61f1f94832fd4b47b81f04bcdd5c317ed36
Mohd Uves Khan <114132833+Uves0786@users.noreply.github.com>
2023-10-06 11:35:28 +0530

    Time: 9 ms (5.93%), Space: 17.1 MB (5.97%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -13,6 +13,7 @@ public:
     output.push_back(element);
     //include call
     subset(nums,output,index+1,ans);
+          output.pop_back();
 }
     vector<vector<int>> subsets(vector<int>& nums)
     {
